2ba25e9a852401f6652f60eb276b65bdda7804ea
Chestnutzzz <57935377+Chestnutzzz@users.noreply.github.com>
2026-03-25 12:51:35 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import './globals.css'
-
+// 在 app/layout.tsx 中
+export const metadata = {
+  title: 'PetCare 智能管家',
+  description: '伴侣生物健康管理专家',
+  // 关键：开启全屏模式
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'PetCare',
+  },
+};
 export default function RootLayout({
   children,
 }: {
